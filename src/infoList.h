2568e2db77314ca1643291827c8a80aa2afa54a2
RiKler <59246000+fdu2019xzy@users.noreply.github.com>
2020-12-02 22:53:12 +0800

    流水线reg输出

--- src/infoList.h
+++ src/infoList.h
@@ -12,6 +12,7 @@
 
 const static std::vector<std::string> StatList = {"None", "AOK", "HLT", "ADR", "INS"};
 const static std::vector<std::string> CCList = {"ZF", "SF", "OF"};
-const static std::vector<std::string> RegList = {"RAX", "RCX", "RDX", "RBX", "RSP", "RBP", "RSI", "RDI", "R8", "R9", "R10", "R11", "R12", "R13", "R14"};
+const static std::vector<std::string> RegList = {"RAX", "RCX", "RDX", "RBX", "RSP", "RBP", "RSI", "RDI", "R8", "R9", "R10", "R11", "R12", "R13", "R14", "None"};
+const static std::vector<std::string> ControlList = {"Normal", "Bubble", "Stall"};
 
 #endif
